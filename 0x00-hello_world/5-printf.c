2187c192030018fb90cee45cc9cb46e58ec30616
Abdiaziz Hassan <zaimyare@gmail.cm>
2023-06-15 13:16:35 -0700

    printf output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  *main - Start point of program or th main functin
+ *   *Return: 0
+ *    * this is my first code
+ *    */
+int main(void)
+
+{
+	        printf("with proper grammar, but the outcome is a piece of art,/n");
+		        return (0);
+}
