16721593421d1d99191b17cb55b5b67c6a5c0eca
Abdiaziz Hassan <zaimyare@gmail.cm>
2023-06-15 13:22:42 -0700

    updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- *  *main - Start point of program or th main functin
- *   *Return: 0
- *    * this is my first code
- *    */
+ *  main - Start point of program or th main functin
+ *  Return: 0
+ *  this is my first code
+ */
 int main(void)
 
 {
-	        printf("with proper grammar, but the outcome is a piece of art,/n");
-		        return (0);
+	rintf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
