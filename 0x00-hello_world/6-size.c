76a044e6fc4c1b595de969bf251464d04da7ee69
Abdiaziz Hassan <zaimyare@gmail.cm>
2023-06-15 13:58:20 -0700

    sizeof diffrent types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point of thr program
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s:)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s:)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s:)\n", sizeof(long int));
+	printf("Size of a long long: %lu byte(s:)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s:)\n", sizeof(float));
+	return (0);
+}
