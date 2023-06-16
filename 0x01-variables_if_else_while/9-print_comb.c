d9547dd3292099abd012672b244f09fe66bc79e2
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-06-16 19:59:50 +0000

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: This code prints possible combinations of digits
+  *Return: 0 always success
+  */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = 0; n2 <= 9; n2++)
+		{
+			putchar('0' + n1);
+			putchar('0' + n2);
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
