62440716c4c27dba219e729bbf57d13d4f737e8a
Abdiaziz Hassan <zaimyare@gmail.cm>
2023-06-16 15:02:26 -0700

    update

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: This code prints possible combinations of digits
+ *Return: 0 always success
+ */
+int main(void)
+{
+	int d1;
+	int d2;
+	int d3 = 0;
+
+	while (d3 < 10)
+	{
+		d2 = 0;
+		while (d2 < 10)
+		{
+			d1 = 0;
+			while (d1 < 10)
+			{
+				if (d1 != d2 && d2 != d3 && d3 < d2 && d2 < d1)
+				{
+					putchar('0' + d3);
+					putchar('0' + d2);
+					putchar('0' + d1);
+					if (d1 + d2 + d3 != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				d1++;
+			}
+			d2++;
+		}
+		d3++;
+	}
+	putchar('\n');
+	return (0);
+}
