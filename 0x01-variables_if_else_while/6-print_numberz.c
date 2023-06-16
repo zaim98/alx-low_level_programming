521e4c48ab60e47741e03daebc96e6b899607197
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-06-16 19:00:16 +0000

    updated

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
-  *main - Entry poit 
-  *Description: This code prints digits of base ten using putchar
-  *Return: 0 always success
+ *main - Entry poit
+ *Description: This code prints digits of base ten using putchar
+ *Return: 0 always success
  */
 int main(void)
 {
 	int n;
+
 	for (n = 0; n <= 9; n++)
 	{
 		putchar(n + 48);
