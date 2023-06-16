8b5b9cfc88c424238085555664308d97b25250fe
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-06-16 17:36:41 +0000

    alphabetic in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - Entry poit or main function
+  *Description: This program display alphabet in lowecase
+  *Return: (0) always success
+*/
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	putchar('\n');
+	return (0);
+}
