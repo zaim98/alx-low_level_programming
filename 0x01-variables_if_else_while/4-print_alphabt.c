0d192186e8584c96385c20ede8950497f8c13f46
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-06-16 18:28:29 +0000

    lowercase axcept e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry poit or main function
+ *Description: This program display alphabet in lowecase
+ *Then skips q and e
+ *Return: (0) always success
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha != 'e' && alpha != 'q')
+		{
+		putchar(alpha);
+		}
+		alpha++;
+	}
+	return (0);
+}
+
