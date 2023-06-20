a258d84ef765daaafd8ebf022ee1e33468e337f6
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-06-20 20:47:09 +0000

    sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: Lists all the natural numbers below 1024 (excluded)
+  *And are also multiples of 3 or 5.
+  *Return: Always 0.
+ */
+int main(void)
+{
+	int n, sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+			sum += n;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
