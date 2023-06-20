6650b55f380a01e425b0d1f2dc55b8ed869f46ba
Abdiaziz Hassan <zaimyare@gmail.cm>
2023-06-20 09:52:05 -0700

    computing absulate value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * Description: coputes the absulate value of integer
+ * Return: absulate value
+ */
+int _abs(int n)
+{
+	return(n <= 0 ? n : n * -1);
+}
