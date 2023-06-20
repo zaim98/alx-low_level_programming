1ec89bf923228309f044d8c7962412e3dd41d5cd
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-06-20 12:54:02 +0000

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *main - Entry point
+  *Description: display alpha in lowercase
+  *Retur: always 0 success
+  */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 122; i++)
+	{
+		_putchar('i');
+		_putchar('\n');
+	}
+	retutn(0);
+}
