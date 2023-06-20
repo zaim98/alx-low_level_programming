66aec6bcd5d6622b2671d9676d14e507da82a8e6
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-06-20 18:53:40 +0000

    adds two numbrs

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  *add - Fuction of the Entry point
+  *@n1: The first int be added.
+  *@n2: second int be added.
+  *Description: adds two numbers
+  *Return: The result of the addition.
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
