f915bead05ac50fdc4a27eb5d9128ec2be1a29e6
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-06-20 19:06:27 +0000

    natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  *print_to_98 - Entry point
+  *Description:Numbers must be separated by a comma, followed by a space
+  *Numbers should be printed in order
+  *The first printed number should be the number passed to your function
+  *The last printed number should be 98
+  *@n: counting begins here
+  */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
