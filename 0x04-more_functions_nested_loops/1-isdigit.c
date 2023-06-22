955c363f804c5beefcbd3699085223c547a5a987
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-06-22 12:33:49 +0000

    updated

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isupper - Entry point
+ * _isdigit - Entry point
  * Description: This program checks if the character is uppercase
  * @c:  the argument number of the  function
  * Return: 1 if uppercase character 0 if it's not
