39f9784b7c7cfe03b082fb31b8e20b44dbb81c95
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-06-22 12:31:40 +0000

    check if its digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * Description: This program checks if the character is uppercase
+ * @c:  the argument number of the  function
+ * Return: 1 if uppercase character 0 if it's not
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
