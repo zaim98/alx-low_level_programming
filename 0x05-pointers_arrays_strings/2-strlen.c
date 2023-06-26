0c6487b5a1f4d833ef13535b043e8e4751079558
Abdiaziz Hassan <zaimyare@gmail.cm>
2023-06-26 05:55:08 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi - Entry
+ * Description: This code Converts a string to an integer.
+ * @s: The string to be converted.
+ * Return: The integer value of the converted string.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int number = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			number = (number * 10) + (*s - '0');
+
+		else if (number > 0)
+			break;
+
+	} while (*s++);
+
+	return (number * sign);
+}
