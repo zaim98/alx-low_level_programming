35474d7ff5b19e5d97487becef45d60370c2de61
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-08-01 11:26:21 +0000

    modified

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,7 +8,7 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t counter = 0;
 
-	while (head && head->next >= head)
+	while (head && head->next <= head)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
 		counter++;
