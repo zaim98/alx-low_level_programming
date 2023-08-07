f30cdd40385d28c2ddeb89b7731dbfa3517215ad
Abdiaziz Hassan <zaimyare98@gmail.com>
2023-08-07 14:00:09 +0000

    Write a program that displays the information contained in the ELF header at the start of an ELF file.

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <elf.h>
 
-void ptitle(char *title);
+void print_title(char *title);
 
 /**
- * errors - prints some errors
+ * print_errors - prints some errors
  * @msg: the error msg
+ *
  * Return: void
  */
-void errors(char *msg)
+void print_errors(char *msg)
 {
 	/* declarations */
 	int len;
@@ -22,14 +23,14 @@ void errors(char *msg)
 }
 
 /**
- * ptype - prints type
- *
+ * print_type - prints type
  * @buffer: contains type info
+ *
  * Return: void
  */
-void ptype(char *buffer)
+void print_type(char *buffer)
 {
-	ptitle("Type");
+	print_title("Type");
 	switch (buffer[6])
 	{
 		case ET_NONE:
@@ -55,15 +56,15 @@ void ptype(char *buffer)
 }
 
 /**
- * pABIversion - prints the ABI version
+ * print_ABI_version - prints the ABI version
  * @buffer: contains the ABI version
  *
  * Return: void
  */
-void pABIversion(char *buffer)
+void print_ABI_version(char *buffer)
 {
 	/* handling the ABI set */
-	ptitle("ABI Version");
+	print_title("ABI Version");
 
 	printf("%d", buffer[8]);
 
@@ -71,10 +72,10 @@ void pABIversion(char *buffer)
 }
 
 /**
- * pentry - prints the ELF entry point address
+ * print_entry - prints the ELF entry point address
  *
  */
-void pentry(void)
+void print_entry(void)
 {
 	Elf64_Ehdr h;
 	int i = 0, len = 0;
@@ -105,15 +106,15 @@ void pentry(void)
 }
 
 /**
- * pos - printing the os/abi
+ * print_OS - printing the os/abi
  * @buffer: contains os/abi info
  *
  * Return: void
  */
-void pos(char *buffer)
+void print_OS(char *buffer)
 {
 	/* handling the OS ABI set */
-	ptitle("OS/ABI");
+	print_title("OS/ABI");
 
 	switch (buffer[7])
 	{
@@ -123,28 +124,204 @@ void pos(char *buffer)
 		case ELFOSABI_HPUX:
 			printf("UNIX - HP-UX");
 			break;
-			errors("Could not read from the file\n");
-			/* checking for elfage */
-			for (i = 0; i < 4; i++)
-			{
-				if (buffer[i] != match[i])
-					errors("Sorry, it's not an ELF file!\n");
-			}
-			/*printing the header! */
-			printf("ELF Header:\n");
-			pmagic(buffer);
-			pclass(buffer);
-			pdata(buffer);
-			pversion(buffer);
-			pos(buffer);
-			pABIversion(buffer);
-			ptype(buffer);
-			pentry();
-			/* close and check for close */
-			if (close(fd))
-				errors("Could not close the file");
+		case ELFOSABI_NETBSD:
+			printf("UNIX - NetBSD");
+			break;
+		case ELFOSABI_LINUX:
+			printf("UNIX - GNU");
+			break;
+		case ELFOSABI_SOLARIS:
+			printf("UNIX - Solaris");
+			break;
+		case ELFOSABI_IRIX:
+			printf("UNIX - IRIX");
+			break;
+		case ELFOSABI_FREEBSD:
+			printf("UNIX - FreeBSD");
+			break;
+		case ELFOSABI_TRU64:
+			printf("UNIX - TRU64");
+			break;
+		default:
+			printf("<unknown: %x>", (unsigned int)buffer[7]);
+	}
+	printf("\n");
+}
 
-			return (0);
+/**
+ * print_version - printing version
+ * @buffer: contains version
+ *
+ * Return: void
+ */
+void print_version(char *buffer)
+{
+	/* handling the version set */
+	print_title("Version");
+
+	switch (buffer[6])
+	{
+		case EV_NONE:
+			printf("0");
+			break;
+		case EV_CURRENT:
+			printf("1 (current)");
+			break;
+		default:
+			printf("%d <unknown: %%lx>", (int)buffer[6]);
 	}
 
+	printf("\n");
+}
 
+/**
+ * print_data - printing the data handling
+ * @buffer: contains data handling info
+ *
+ * Return: void
+ */
+void print_data(char *buffer)
+{
+	/* handling the data set */
+	print_title("Data");
+
+	switch (buffer[5])
+	{
+		case ELFDATANONE:
+			printf("none");
+			break;
+		case ELFDATA2LSB:
+			printf("2's complement, little endian");
+			break;
+		case ELFDATA2MSB:
+			printf("2's complement, big endian");
+			break;
+		default:
+			printf("<unknown: %x>", (unsigned int)buffer[5]);
+	}
+
+	printf("\n");
+}
+
+/**
+ * print_magic - printing the Magic we need
+ * @buffer: contains magic
+ *
+ * Return: void
+ */
+void print_magic(char *buffer)
+{
+	/* declarations */
+	int i;
+
+	/* handling the magics */
+	printf("  Magic:   ");
+	for (i = 0; i < 16; i++)
+		printf("%.2x ", buffer[i]);
+	printf("\n");
+}
+
+/**
+ * print_class - printing the class, in class
+ * @buffer: the buffer containing class or none
+ *
+ * Return: void
+ */
+void print_class(char *buffer)
+{
+	/* handling the class */
+	print_title("Class");
+
+	switch (buffer[4])
+	{
+		case ELFCLASSNONE:
+			printf("none");
+			break;
+		case ELFCLASS32:
+			printf("ELF32");
+			break;
+		case ELFCLASS64:
+			printf("ELF64");
+			break;
+		default:
+			printf("<unknown: %x>", (unsigned int)buffer[4]);
+	}
+	printf("\n");
+}
+
+/**
+ * print_title - printing titles since 2020
+ * @title: the title
+ *
+ * Return: void
+ */
+void print_title(char *title)
+{
+	/* declarations */
+	int size = 37;
+	int len, i;
+
+	/* handling the titles & spaces */
+	printf("  ");
+	printf("%s:", title);
+
+	for (len = 0; title[len]; len++)
+		;
+
+	for (i = 0; i < size - 3 - len; i++)
+		printf(" ");
+}
+
+/**
+ * main - print an ELF header and such
+ * @argc: count of args
+ * @argv: vector of args
+ *
+ * Return: int or exit code
+ */
+int main(int argc, char **argv)
+{
+	/* declarations */
+	int fd, readVal, i;
+	char buffer[16];
+	char match[4] = {0x7f, 'E', 'L', 'F'};
+
+	/* too many args? too few? */
+	if (argc != 2)
+		print_errors("Improper usage\n");
+
+	/* file descriptor, opening */
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+
+		print_errors("Could not open the file\n");
+
+	/* reading */
+	readVal = read(fd, buffer, 16);
+	if (readVal == -1)
+		print_errors("Could not read from the file\n");
+
+	/* checking for elfage */
+	for (i = 0; i < 4; i++)
+	{
+		if (buffer[i] != match[i])
+			print_errors("Sorry, it's not an ELF file!\n");
+	}
+
+	/*printing the header! */
+	printf("ELF Header:\n");
+	print_magic(buffer);
+	print_class(buffer);
+	print_data(buffer);
+	print_version(buffer);
+	print_OS(buffer);
+	print_ABI_version(buffer);
+	print_type(buffer);
+	print_entry();
+
+	/* close and check for close */
+	if (close(fd))
+		print_errors("Could not close the file");
+
+	return (0);
+}
